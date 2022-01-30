1694ed19f447d11a7090c31d7442038f9a214d15
gihozoinnocente <igihozoinnocente@gmail.com>
2022-01-30 17:45:16 +0200

    Added changes on imageUpload

---
+++ src/helpers/imageUpload.js
@@ -0,0 +1,17 @@
+import cloudinary from 'cloudinary'
+export const uploadFile = async (req) => {
+    let imageUrl = ''
+    await cloudinary.v2.uploader.upload(req.file.path, async function (err, image) {
+        if (err) { console.warn(err); }
+        imageUrl = image.url
+    });
+    return imageUrl
+}
+
+
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_USER_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_USER_NAME
+})
