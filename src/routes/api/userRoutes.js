8b5cad400973ba861ee739833cce3bfec46bbce5
gihozoinnocente <igihozoinnocente@gmail.com>
2022-01-31 14:59:41 +0200

    Added comment section

--- src/routes/api/userRoutes.js
+++ src/routes/api/userRoutes.js
@@ -1,14 +1,3 @@
-// import express from 'express'
-
-// const route= express.Router()
-
-// route.get('/', (req, res, next) => {
-//     res.status(200).json({ status: 200, message: "this will return all users", data: "" })
-// })
-
-// export default route
-
-
 import express from 'express'
 import  UserController from './../../controllers/userController.js'
 const route = express.Router()
