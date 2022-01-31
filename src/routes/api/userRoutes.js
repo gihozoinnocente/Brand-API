8331dbf91f7f185cb64b12e764ed9126d5a79039
gihozoinnocente <igihozoinnocente@gmail.com>
2022-01-31 15:43:21 +0200

    user validation

--- src/routes/api/userRoutes.js
+++ src/routes/api/userRoutes.js
@@ -1,21 +1,16 @@
 import express from 'express'
-import { fileFilter } from '../../helpers/fileFilter.js'
 import multer from 'multer';
-import  UserControllers from '../../controllers/userController.js';
+import { UserControllers } from '../../controllers/userController.js';
+import { fileFilter } from '../../helpers/fileFilter.js';
+//import { userValidation } from '../../validations/userValidation/user.validation.js';
 
 const route = express.Router()
-
-// route.get('/', (req, res, next) => {
-//     res.status(200).json({ status: 200, message: "this will return all users", data: "" })
-// })
-
-
 const storage = multer.diskStorage({});
-const uploads = multer({ storage,fileFilter});
 
-const userControllers =new UserControllers()
-
-route.post('/register',uploads.single('picture'),userControllers.register)
-route.post('login',userControllers.login)
+const uploads = multer({ storage, fileFilter });
+const userControllers = new UserControllers()
+//route.post('/register', uploads.single('picture'), userValidation, userControllers.register)
+route.post('/register', uploads.single('picture'), userControllers.register)
+route.post('/login', userControllers.login)
 
 export default route
