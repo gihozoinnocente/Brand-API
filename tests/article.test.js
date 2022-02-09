dad6f9e33cab9d46c2a63f2d549a481de5ab1bc1
gihozoinnocente <igihozoinnocente@gmail.com>
2022-02-09 16:25:29 +0200

    changes on test-articles

--- tests/article.test.js
+++ tests/article.test.js
@@ -1,75 +1,3 @@
-// import chai, { expect } from 'chai'
-// import chaiHttp from 'chai-http'
-// import app from '../src/app.js'
-// import 'dotenv/config';
-// import Article from "./../src/models/article.js"
-
-// let articleId
-
-// const getArticleId= async ()=>{
-//     const all = await Article.find()
-//     let id=all[0]._id;
-//     return id
-// }
-
-// (async ()=>{
-//     articleId=await getArticleId()
-// })()
-
-// chai.use(chaiHttp)
-// describe("ARTICLE END-POINT TESTING", () => {
-//     it("Should retrieve the articles", (done) => {
-//         chai.request(app).get("/api/v1/articles/")
-//         .send()
-//         .end((err,res)=>{
-//             expect(res).to.have.property("status")
-//             expect(res.body).to.have.property("message")
-//             expect(res.body).to.have.property("data")
-//           done()
-//         })
-        
-//     })
-//     it("Should not retrieve the articles",  (done) => {
-//         chai.request(app).get("/api/v1/aritcle/")
-//         .send()
-//         .end((err,res)=>{
-//         expect(res).to.have.status([404])
-//         done()
-//     })
-//     })
-//     it("Should  retrieve the article by id", (done) => {
-//         chai
-//             .request(app)
-//             .get(`/api/v1/articles/${articleId}`)
-//             .send()
-//             .end((err, res) => {
-//                 expect(res).to.have.status([200]);
-//                 expect(res).to.have.property("status");
-//                 expect(res.body).to.have.property("message");
-//                 expect(res.body).to.be.a("object");
-//                 done();
-//             });
-//     });
-
-//     it("Should  delete the article by id", (done) => {
-//         chai
-//             .request(app)
-//             .delete(`/api/v1/articles/${articleId}`)
-//             .send()
-//             .end((err, res) => {
-//                 expect(res).to.have.status([200]);
-//                 expect(res).to.have.property("status");
-//                 expect(res.body).to.have.property("message");
-//                 expect(res.body).to.be.a("object");
-//                 done();
-//             });
-//     });
-
-
- 
-// })
-
-
 import chai, { expect } from 'chai'
 import chaiHttp from 'chai-http'
 import app from '../src/app.js'
@@ -77,8 +5,6 @@ import 'dotenv/config';
 import { article, userData, validUser } from './dummyData.js';
 import Article from "./../src/models/article.js"
 import { array } from 'joi';
-//import User from "./../src/models/user.js"
-
 
 let articleId
 
