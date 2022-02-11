3f8d3ebe081270dbb254887381210124b74d15a6
gihozoinnocente <igihozoinnocente@gmail.com>
2022-02-11 19:16:23 +0200

    users test

--- tests/dummyData.js
+++ tests/dummyData.js
@@ -25,3 +25,36 @@ export const query={
     email:"igihozoinnocente@gmail.com",
     message:"This is a query"
 }
+
+
+
+export const validUser2={
+    email:"ihonore456@gmail.com",
+    password:"Password2!"
+}
+export const userData2={
+    username:"Gihozo",
+    email:"gihozo5@gmail.com",
+    password:"pass@123!"
+}
+
+export const validUser3={
+    email:"irad@gmail.com",
+    password:"Password3!"
+}
+export const userData3={
+    username:"innocente",
+    email:"innocente5@gmail.com",
+    password:"pass@123"
+}
+
+export const validUsertoDelete={
+    email:"inno@yahoo.fr",
+    password:"Password4!"
+}
+export const userDatatoDelete={
+    username:"Gihozo",
+    email:"inno@yahoo.fr",
+    password:"Password4!"
+}
+
